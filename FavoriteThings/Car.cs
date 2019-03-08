6c3893db85ffa680151a9b7e62658fabacc97be4
Lance Hamilton <lance.hamilton24@gmail.com>
2019-03-08 09:39:09 -0600

    added favorite car

---
+++ FavoriteThings/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings
+{
+    class Car
+    {
+        //Properties
+        public string Make { get;  }
+        public CarType CarType { get; }
+
+        //fields 
+
+        //constructors
+        public Car(string make, CarType cartype)
+        {
+            Make = make;
+            CarType = cartype; 
+        }
+
+        //methods
+        public string Offer()
+        {
+            return $"I want a {CarType} made by {Make}";
+        }
+        public string Rejection()
+        {
+            return $"I don't want your {Make}";
+        }
+    }
+
+    internal enum CarType
+    {
+        Suv,
+        Coupe,
+        Hatchback,
+        Truck,
+        Semitruck,
+    }
+}
